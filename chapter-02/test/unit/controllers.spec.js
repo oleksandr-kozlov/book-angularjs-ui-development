373d9d6776a73755f84344a7e8448cbc1d451e99
Olexander Kozlov <olek@isd.dp.ua>
2015-04-22 14:51:43 +0300

    chapter-02

---
+++ chapter-02/test/unit/controllers.spec.js
@@ -0,0 +1,18 @@
+(function() {
+   'use strict';
+
+   describe('controller specs', function() {
+      var $scope;
+
+      beforeEach(module('myApp.controllers'));
+
+      beforeEach(inject(function($rootScope, $controller) {
+         $scope = $rootScope.$new();
+         $controller('helloWorldCtrl', {$scope: $scope});
+      }));
+
+      it('should create "name" model with first name "Jane"', function() {
+         expect($scope.name.first).toBe("Jane");
+      });
+   });
+}());
